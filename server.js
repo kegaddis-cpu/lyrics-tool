bd8fe334b7717a8359bec046366a681967544b44
kegaddis-cpu <kegaddis@gmail.com>
2026-06-28 15:45:03 -0400

    Refactor server.js to improve database handling

--- server.js
+++ server.js
@@ -310,9 +310,9 @@ app.use((err, req, res, next) => {
   res.status(500).send("Server error");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
 
